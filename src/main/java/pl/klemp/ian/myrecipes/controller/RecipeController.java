a79967d8e04f7046bb1ed2975ddaaceaa74e46bb
Ian Klemp <klemp.ian@gmail.com>
2021-01-05 20:54:19 +0100

    find recipes by keyword

--- src/main/java/pl/klemp/ian/myrecipes/controller/RecipeController.java
+++ src/main/java/pl/klemp/ian/myrecipes/controller/RecipeController.java
@@ -13,13 +13,15 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import pl.klemp.ian.myrecipes.dto.RecipeDto;
 import pl.klemp.ian.myrecipes.dto.RecipeThumbnailDto;
-import pl.klemp.ian.myrecipes.model.Category;
+import pl.klemp.ian.myrecipes.model.Keyword;
 import pl.klemp.ian.myrecipes.model.Recipe;
-import pl.klemp.ian.myrecipes.service.CategoryService;
+import pl.klemp.ian.myrecipes.service.KeywordService;
 import pl.klemp.ian.myrecipes.service.RecipeService;
 
 import javax.validation.Valid;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
@@ -27,8 +29,8 @@ import java.util.stream.Collectors;
 @RestController
 public class RecipeController {
 
-    private final CategoryService categoryService;
     private final RecipeService recipeService;
+    private final KeywordService keywordService;
     private final ModelMapper modelMapper;
 
     @ResponseStatus(HttpStatus.CREATED)
@@ -52,6 +54,16 @@ public class RecipeController {
         return recipeService.findAllByRecipeCategoryId(id).stream()
                 .map(recipe -> modelMapper.map(recipe, RecipeThumbnailDto.class))
                 .collect(Collectors.toList());
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/keyword")
+    public List<RecipeThumbnailDto> getByKeyword(@RequestParam String key) {
+        Optional<Keyword> optionalKeyword = keywordService.findByName(key);
+
+        return optionalKeyword.map(keyword -> recipeService.findAllByKeyword(keyword).stream()
+                .map(recipe -> modelMapper.map(recipe, RecipeThumbnailDto.class))
+                .collect(Collectors.toList())).orElse(Collections.emptyList());
     }
 
     @ResponseStatus(HttpStatus.OK)
